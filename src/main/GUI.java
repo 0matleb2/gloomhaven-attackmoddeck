9c7664dc65195646786670ca548afa4db1fbb829
Mathieu Leblanc <0mat.leb2@gmail.com>
2018-04-06 17:00:56 -0400

    Add button depression animation [Story #5][Task #6]

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -7,26 +7,31 @@ import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 import java.awt.Font;
+import java.awt.Point;
 import java.awt.Color;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.border.BevelBorder;
 
 import cards.AttackModifierDeck;
 import contracts.IAttackModifierCard;
 import contracts.IAttackModifierDeck;
 
-import javax.swing.JMenuBar;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 import java.awt.Toolkit;
 
 public class GUI {
 
 	private JFrame frmSnowblindsGloomhavenAttack;
+	private ArrayList<JLabel> drawnCardLabels;
+	private JLabel lblShuffle;
 	private IAttackModifierDeck deck;
 
 	/**
@@ -49,8 +54,9 @@ public class GUI {
 	 * Create the application.
 	 */
 	public GUI() {
-		initialize();
 		deck = new AttackModifierDeck();
+		drawnCardLabels = new ArrayList<JLabel>();
+		initialize();
 	}
 
 	/**
@@ -70,11 +76,16 @@ public class GUI {
 		frmSnowblindsGloomhavenAttack.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
+		lblShuffle = new JLabel("");
+		lblShuffle.setIcon(new ImageIcon(GUI.class.getResource("/icons/Reshuffle.png")));
+		lblShuffle.setBounds(484, 603, 47, 46);
+		panel.add(lblShuffle);
+		
 		JLabel lblTitle = new JLabel("Attack Modifier Deck Simulator");
 		lblTitle.setForeground(new Color(255, 245, 238));
 		lblTitle.setFont(new Font("Pirata One", Font.PLAIN, 36));
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTitle.setBounds(126, 228, 767, 39);
+		lblTitle.setBounds(126, 227, 767, 39);
 		panel.add(lblTitle);
 		
 		JLabel lblSnowblind = new JLabel("Snowblind's");
@@ -89,7 +100,9 @@ public class GUI {
 		lblBless.setHorizontalAlignment(SwingConstants.CENTER);
 		lblBless.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/Double Damage - Bless.png")));
 		lblBless.setBounds(297, 294, 437, 296);
+		drawnCardLabels.add(lblBless);
 		lblBless.setVisible(false);
+		
 		panel.add(lblBless);
 		
 		JLabel lblDoubleDamage = new JLabel("");
@@ -98,6 +111,7 @@ public class GUI {
 		lblDoubleDamage.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/Double Damage.png")));
 		lblDoubleDamage.setBounds(297, 294, 437, 296);
 		lblDoubleDamage.setVisible(false);
+		drawnCardLabels.add(lblDoubleDamage);
 		panel.add(lblDoubleDamage);
 		
 		JLabel lblPlusTwo = new JLabel("");
@@ -106,6 +120,7 @@ public class GUI {
 		lblPlusTwo.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/Plus 2.png")));
 		lblPlusTwo.setBounds(297, 294, 437, 296);
 		lblPlusTwo.setVisible(false);
+		drawnCardLabels.add(lblPlusTwo);
 		panel.add(lblPlusTwo);
 		
 		JLabel lblPlusOne = new JLabel("");
@@ -114,6 +129,7 @@ public class GUI {
 		lblPlusOne.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/Plus 1.png")));
 		lblPlusOne.setBounds(297, 294, 437, 296);
 		lblPlusOne.setVisible(false);
+		drawnCardLabels.add(lblPlusOne);
 		panel.add(lblPlusOne);
 		
 		JLabel lblNetural = new JLabel("");
@@ -122,6 +138,7 @@ public class GUI {
 		lblNetural.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/Neutral.png")));
 		lblNetural.setBounds(297, 294, 437, 296);
 		lblNetural.setVisible(false);
+		drawnCardLabels.add(lblNetural);
 		panel.add(lblNetural);
 		
 		JLabel lblMinusOne = new JLabel("");
@@ -130,6 +147,7 @@ public class GUI {
 		lblMinusOne.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/Minus 1.png")));
 		lblMinusOne.setBounds(297, 294, 437, 296);
 		lblMinusOne.setVisible(false);
+		drawnCardLabels.add(lblMinusOne);
 		panel.add(lblMinusOne);
 		
 		JLabel lblMinusTwo = new JLabel("");
@@ -138,6 +156,7 @@ public class GUI {
 		lblMinusTwo.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/Minus 2.png")));
 		lblMinusTwo.setBounds(297, 294, 437, 296);
 		lblMinusTwo.setVisible(false);
+		drawnCardLabels.add(lblMinusTwo);
 		panel.add(lblMinusTwo);
 		
 		JLabel lblMiss = new JLabel("");
@@ -146,6 +165,7 @@ public class GUI {
 		lblMiss.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/No Damage.png")));
 		lblMiss.setBounds(297, 294, 437, 296);
 		lblMiss.setVisible(false);
+		drawnCardLabels.add(lblMiss);
 		panel.add(lblMiss);
 		
 		JLabel lblCurse = new JLabel("");
@@ -154,6 +174,7 @@ public class GUI {
 		lblCurse.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/No Damage - Curse.png")));
 		lblCurse.setBounds(297, 294, 437, 296);
 		lblCurse.setVisible(false);
+		drawnCardLabels.add(lblCurse);
 		panel.add(lblCurse);
 		
 		JLabel lblCardBlank = new JLabel("");
@@ -162,6 +183,7 @@ public class GUI {
 		lblCardBlank.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/Attack Modifier - Blank.jpg")));
 		lblCardBlank.setBounds(297, 294, 437, 296);
 		lblCardBlank.setVisible(false);
+		drawnCardLabels.add(lblCardBlank);
 		panel.add(lblCardBlank);
 		
 		JLabel lblCardBlankReshuffle = new JLabel("");
@@ -170,6 +192,7 @@ public class GUI {
 		lblCardBlankReshuffle.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/Attack Modifier - Blank - Reshuffle.jpg")));
 		lblCardBlankReshuffle.setBounds(297, 294, 437, 296);
 		lblCardBlankReshuffle.setVisible(false);
+		drawnCardLabels.add(lblCardBlankReshuffle);
 		panel.add(lblCardBlankReshuffle);
 		
 		JLabel lblCardBack = new JLabel("");
@@ -177,6 +200,7 @@ public class GUI {
 		lblCardBack.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCardBack.setIcon(new ImageIcon(GUI.class.getResource("/attackModifers/Attack Modifier - Back.jpg")));
 		lblCardBack.setBounds(297, 294, 437, 296);
+		drawnCardLabels.add(lblCardBack);
 		panel.add(lblCardBack);
 		
 		JButton btnDraw = new JButton("");
@@ -246,18 +270,69 @@ public class GUI {
 			}
 		});
 		
+		btnDraw.addMouseListener(new MouseListener() {
+			private Point location = drawnCardLabels.get(0).getLocation();
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				for (JComponent drawnCardLabel : drawnCardLabels) {
+					moveJComponent(new Point(location.x, location.y + 1), drawnCardLabel);	
+				}		
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				for (JComponent drawnCardLabel : drawnCardLabels) {
+					moveJComponent(location, drawnCardLabel);	
+				}				
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) { }
+
+			@Override
+			public void mouseExited(MouseEvent e) { }
+
+			@Override
+			public void mouseClicked(MouseEvent e) { }
+		});
+		
 		btnDraw.setFont(new Font("Pirata One", Font.PLAIN, 36));
 		btnDraw.setForeground(new Color(255, 245, 238));
 		btnDraw.setOpaque(false);
 		btnDraw.setContentAreaFilled(false);
-		btnDraw.setBorderPainted(true);
+		btnDraw.setBorderPainted(false);
 		btnDraw.setFocusPainted(false);
 		btnDraw.setBorder(new BevelBorder(BevelBorder.RAISED, new Color(255, 245, 238), new Color(245, 222, 179), null, null));
 		btnDraw.setBounds(297, 294, 437, 296);
 		panel.add(btnDraw);
 		
 		CircleButton btnShuffle = new CircleButton("");
+		btnShuffle.addMouseListener(new MouseListener() {
+			private Point location = lblShuffle.getLocation();
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				moveJComponent(new Point(location.x, location.y + 1), lblShuffle);			
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				moveJComponent(location, lblShuffle);				
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) { }
+
+			@Override
+			public void mouseExited(MouseEvent e) { }
+
+			@Override
+			public void mouseClicked(MouseEvent e) { }
+		});
+		
 		btnShuffle.addActionListener(new ActionListener() {
+
 			public void actionPerformed(ActionEvent e) {
 				deck.shuffle();
 				
@@ -288,10 +363,14 @@ public class GUI {
 		btnShuffle.setBounds(475, 602, 64, 46);
 		panel.add(btnShuffle);
 		
-		JLabel lblShuffle = new JLabel("");
-		lblShuffle.setIcon(new ImageIcon(GUI.class.getResource("/icons/Reshuffle.png")));
-		lblShuffle.setBounds(484, 603, 47, 46);
-		panel.add(lblShuffle);
-		
 	}
+	
+	private void moveJComponent(final Point p, JComponent component) {
+	    SwingUtilities.invokeLater(new Runnable() {
+	      @Override
+	      public void run() {
+	    	  component.setLocation(p);
+	      }
+	    });
+	  }
 }
